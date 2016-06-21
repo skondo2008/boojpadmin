2522012cf0664b4f685cd13391ae9aedf9864b82
skondo <skondo2008@gmail.com>
2016-06-21 14:58:03 +0800

    仮デモ

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/home', ['middleware' => 'auth', 'uses' => 'DashboardController@index']);
+Route::get('/home/{id}', ['middleware' => 'auth', 'uses' => 'DashboardController@show']);
 /* ログイン画面の表示 */
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 /* ログイン処理 */
@@ -31,3 +32,16 @@ Route::post('password/email', 'Auth\PasswordController@postEmail');
 // パスワードリセットルート
 Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
 Route::post('password/reset', 'Auth\PasswordController@postReset');
+
+//
+//Route::get('users','UsersController@index');
+//Route::get('users/create','UsersController@create');
+//Route::post('users/store','UsersController@store');
+
+//
+//Route::get('articles', 'ArticlesController@index');
+//Route::get('articles/{id}', 'ArticlesController@show');
+
+//Route::get('bookinginfos', 'BookingInfosController@index');
+//Route::get('bookinginfos/{id}', 'BookingInfosController@show');
+    
