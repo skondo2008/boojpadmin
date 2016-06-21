8d31b4c846dec182dbe5e84a68411ed49d4a751f
skondo <skondo2008@gmail.com>
2016-06-21 14:49:29 +0800

    仮デモ

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,23 @@
+<!-- resouces/views/articles/index.blade.php -->
+ 
+@extends('layout')
+ 
+@section('content')
+    <h1>Articles</h1>
+ 
+    <hr/>
+     
+    @foreach($articles as $article)
+        <article>
+            <h2>
+                <a href="{{ url('articles', $article->id) }}">
+                    {{ $article->title }}
+                </a>
+            </h2>
+            <div class="body">
+                {{ $article->body }}
+            </div>
+        </article>
+    @endforeach
+@endsection
+
