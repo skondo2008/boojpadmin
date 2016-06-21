ed38219a6411b335c8332d390afe68a809812030
skondo <skondo2008@gmail.com>
2016-06-21 14:48:04 +0800

    仮デモ

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,14 +1,51 @@
 {{-- resources/views/auth/index.blade.php --}}
 @extends('auth')
 @section('content')
-                <div class="page-header">
-                    <div class="container">
-                        <h2>Dashboard</h2>
-                    </div>
-                </div>
                 <?php $user = Auth::user() ?>
                 <div class="container">
-                    こんにちは {{ $user['name'] }} さん
+                    こんにちはは {{ $user['name'] }} さん
                     </div>
                 </div>
+                <div class="container">
+                    <!-- table -->
+                    <table class="table table-striped">
+
+                    <!-- loop -->
+                    @foreach($bookinginfos as $bookinginfo)
+                    <tr>
+                    <td>
+                        <a href="{{ url('home', $bookinginfo->orderId) }}" target="_blank">
+                            {{ $bookinginfo->orderId }}
+                        </a>
+                    </td>
+                    <td>
+                        {{ $bookinginfo->resalerCd }}
+                    </td>
+                    <td>
+                        {{ $bookinginfo->resalerOrderNum }}
+                    </td>
+                    <td>
+                        {{ $bookinginfo->resalarHotelId }}
+                    </td>
+                    <td>
+                        {{ $bookinginfo->providerCd }}
+                    </td>
+                    <td>
+                        {{ $bookinginfo->bookingRef }}
+                    </td>
+                    <td>
+                        {{ $bookinginfo->totalPrice }}
+                    </td>
+                    <td>
+                        {{ $bookinginfo->checkin }}
+                    </td>
+                    <td>
+                        {{ $bookinginfo->checkout }}
+                    </td>
+                    </tr>
+                @endforeach
+                    </table>
+                <!-- page control -->
+                {!! $bookinginfos->render() !!}
 @endsection
+
